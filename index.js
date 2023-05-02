5a1f5f78b1939a77353fd2aa82963fdaf94b939c
gilangwokeh <gilangwokeh12@gmail.com>
2023-05-02 12:46:02 +0700

    update

---
+++ index.js
@@ -0,0 +1,116 @@
+// function kurang(nilai1,nilai2){
+//     return nilai1 - nilai2
+// }
+// function kali(nilai1,nilai2){
+//     return nilai1 * nilai2
+// }
+
+// function bagi(nilai1,nilai2){
+//     return nilai1 / nilai2
+// }
+// function tambah(nilai1,nilai2){
+//     return nilai1 + nilai2
+// }
+
+// function calculate(nilai1,nilai2,arg){
+//     switch(arg){
+//         case "tambah" :
+//         return  tambah(nilai1,nilai2)
+//         case "kurang" :
+//         return  kurang(nilai1,nilai2)
+//         case "kali" :
+//         return  kali(nilai1,nilai2)
+//         case "bagi" :
+//         return  bagi(nilai1,nilai2)
+//     }
+// }
+
+// console.log(calculate(30,123,'tambah'))
+
+// console.log(kurang(6,102))
+// console.log(kali(140,11))
+// console.log(bagi(10,2))
+// let number = 10;
+// if (number === 1){
+//     console.log('benar')
+// }else if(number === 2){
+//      console.log('benar')
+// }else {
+//     console.log('salah')
+// }
+
+// let fruit = "pepaya";
+// switch (fruit) {
+//   case "orange":
+//     console.log("anda memakan buah orange");
+//   case "melon":
+//     console.log("anda memakan buah melon");
+//   case "anggur":
+//     console.log("anda memakan buah semangka");
+//   case "pepaya":
+//     console.log("anda memakan buah jeruk");
+//     break;
+//     default:
+//         console.log('buah yanng anda makan tidak ada')
+// }
+
+// const age = 18;
+
+// console.log(age === '18' ? 'benar' : "salah")
+
+// let i = 0;
+// while (i < 10){
+//     console.log(i);
+//     i++;
+// }
+
+// do {
+//     console.log('benar')
+// }
+// while (false);
+
+// for (let  l = 4; l < 10; l++){
+//     console.log(l)
+// }
+
+// let nums = [3,2,1];
+
+// for(let num in nums){
+//     console.log(nums,nums[num])
+// }
+
+// for (let num of nums){
+//     console.log(num)
+// }
+
+// let kutu = 1 ;
+// while (kutu <= 10){
+//     console.log('benar kutu ada berapa ' + kutu);
+//     kutu++
+// }
+
+// object
+// const car =  {
+//     type: "flat",
+//     model: 5000,
+//     color: "white"
+// }
+
+// let text = 'apple, banana, kiwi';
+// let part = text.slice(-12 ,-6)
+
+// let x = 0.2 + 0.1;
+// document.getElementById('demo').innerHTML = "0.2 + 0.1 = " + x;
+// let y = (0.2*10 + 0.1*10)/10;
+
+// document.getElementById("demo1").innerHTML = "0.2 + 0.1 = " +y
+
+// for (const x of tags){
+//     html += `<li>${x}</li>`;
+// }
+
+// html += `<ul></ul>`
+
+// let x = 123;
+
+// document.getElementById("demo").innerHTML = x.toString() + "<br>" + (123).toString() + "<br>" + (100 + 23).toString(); 
